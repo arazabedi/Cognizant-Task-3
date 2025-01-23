6f9e2132b6f2671dcaed60cc0531a0c30b8f67e7
Araz <112658492+arazabedi@users.noreply.github.com>
2025-01-23 18:16:04 +0000

    Update and rename Plugin.cs to PreventMultipleCasesPlugin.cs

--- PreventMultipleCasesOnAccountPlugin/PreventMultipleCasesPlugin.cs
+++ PreventMultipleCasesOnAccountPlugin/PreventMultipleCasesPlugin.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,29 +7,42 @@ using System.Threading.Tasks;
 using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Query;
 
-namespace PreventMultipleCasesOnAccountPlugin
+namespace Plugins
 {
-    public class Plugin : IPlugin
+    public class PreventMultipleCasesPlugin : IPlugin
     {
         public void Execute(IServiceProvider serviceProvider)
         {
             // Get the execution context
-            IPluginExecutionContext context = (IPluginExecutionContext) serviceProvider.GetService(typeof(IPluginExecutionContext));
+            IPluginExecutionContext context = serviceProvider.GetService(typeof(IPluginExecutionContext)) as IPluginExecutionContext;
+
+            // Check that context exists and it contains the Target entity
+            if (context == null || context.InputParameters.Contains("Target = false") || !(context.InputParameters["Target"] is Entity))
+            {
+                return;
+            }
 
             // Get the organisation service
-            IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory) serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationServiceFactory serviceFactory = serviceProvider.GetService(typeof(IOrganizationServiceFactory)) as IOrganizationServiceFactory;
             IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
 
+                
             // Get the tracing service
-            ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            ITracingService tracingService = serviceProvider.GetService(typeof(ITracingService)) as ITracingService;
 
             try
             {
                 // Get the case entity (also referred to as incident in the Plugin Registration Tool)
                 Entity caseEntity = (Entity) context.InputParameters["Target"];
 
+                // Check that customerid attribute is present and of type EntityReference
+                if (!caseEntity.Attributes.Contains("customerid") || !(caseEntity["customerid"] is EntityReference))
+                {
+                    return;
+                }
+
                 // Get the customer id (account or contact - it doesn't matter)
-                EntityReference customerRef = (EntityReference) caseEntity ["customerid"];
+                EntityReference customerRef = caseEntity ["customerid"] as EntityReference;
 
                 // Get the customer id
                 Guid customerId = customerRef.Id;
@@ -43,6 +56,7 @@ namespace PreventMultipleCasesOnAccountPlugin
                 }
             } catch (Exception ex)
             {
+                // Rethrow to ensure exception is logged with error details traced 
                 tracingService.Trace("PreventMultipleCasesOnAccountPlugin: {0}", ex.ToString());
                 throw;
             }
@@ -52,10 +66,11 @@ namespace PreventMultipleCasesOnAccountPlugin
         // Checks whether there are open cases
         private bool ContainsOpenCases(IOrganizationService service, Guid customerId)
         {
-            // Query to check for open cases
+            // Query to check for one open case
             QueryExpression query = new QueryExpression("incident")
             {
-                ColumnSet = new ColumnSet("statecode", "customerid"),
+                // ColumnSet does not retrieve attribute data (as only the existence of a record is needed)
+                ColumnSet = new ColumnSet(false),
                 Criteria = new FilterExpression()
                 {
                     Conditions =
@@ -63,22 +78,23 @@ namespace PreventMultipleCasesOnAccountPlugin
                         new ConditionExpression("statecode", ConditionOperator.Equal, 0),
                         new ConditionExpression("customerid", ConditionOperator.Equal, customerId)
                     }
-                }
+                },
+                // Limit to retrieve just 1 record for efficiency
+                TopCount = 1
             };
 
-            // Use query to get collection containing all open cases
+            // Use query to get a collection either containing a single case or none
             EntityCollection openCases = service.RetrieveMultiple(query);
             
-            // Check if collection has any cases
+            // Check if collection has a case
             if (openCases.Entities.Count > 0)
             {
-                // Contains open cases
+                // Contains an open case
                 return true;
-            } else
-            {
-                // Doesn't contain open cases
-                return false;
             }
+
+            // Doesn't contain an open case
+            return false;
         }
 
     }
